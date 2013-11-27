bcddeac4ac4d4a9871bd6d1c6630a9b7d23658e8
BombardaIllegale <cri.and23@gmail.com>
2013-11-28 00:42:27 +0100

    Manca Poco!!

--- CollisioneRangeTorretta.js
+++ CollisioneRangeTorretta.js
@@ -1,28 +1,89 @@
 ﻿
 
+//Funziona tutto, con una o piu torrette i nemici vengono eliminati in ordine di apparizione, manca da implementare il cambio di target se un nemico ne supera un altro in velocita
+
+var array_nemici = new Array();
+var proiettile : GameObject;
+var enemy : GameObject;
+var vita : EnemyHP;
+var nextFire = 1;
+var i = 0;
 
 function OnTriggerEnter2D(collision : Collider2D){
 
+if (collision.gameObject.name != "Torre1"){//questa condizione serve fa in modo che nessuna torretta diventi un enemy
+
+array_nemici.Push(collision.gameObject);
+
+}
 
 
 }
 
-//la function Identify enemy andra nell Update()
-function IdentifyEnemy(){
-//aggiungo ad un array ogni enemy che entra nella collision
-//faccio un cilclo for che cerca prende la posizione tra tutti <code> array_nemici[i].transform.position.x </code> le posizioni vengono confrontate e quello con la x piu grande diventa l obbiettivo della torretta
-//una volta preso l obbiettivo la torretta controlla che sia nel suo range <code> Vector3.Distance(gameObject.transform.position, enemy.transform.position) < n </code> e che la vita del gameObject sia diversa da 0
-//la torretta istanzia il proiettile che si muove contro il nemico selezionato, la vita viene diminuita
-//controllo che la vita sia = 0,in questo caso distruggo il nemico e lo rimuovo dall array
-//ricomputo il for e faccio tutto da capo 
+
+
+function OnTriggerExit2D(collision : Collider2D){
+if (array_nemici.length > 0){
+array_nemici.RemoveAt(0);
+}
+Debug.Log("Hello");
 
 
 
 }
+
+
+
 
 function Update(){
 
+if (array_nemici.length > 0){
 
-
+  enemy = array_nemici[0];
   
+  if (enemy == null){
+
+    array_nemici.RemoveAt(0);
+
+  }else{
+
+    vita = enemy.GetComponent(EnemyHP);
+
+  }
+
+  if(Time.time > nextFire){
+
+    if(vita.hp != 0){
+
+
+      var clone = Instantiate(proiettile, Vector3(transform.position.x, transform.position.y+0.5, transform.position.z), transform.rotation);
+      
+      iTween.MoveTo(clone, Vector3(enemy.transform.position.x-0.3, enemy.transform.position.y, enemy.transform.position.z), 1);
+    
+        nextFire = Time.time + 1;
+          
+                    
+        Destroy(clone,0.3f);
+        vita.hp--;
+        
+          
+        if(vita.hp == 0){
+          Destroy(enemy, 0.2f);
+          
+            array_nemici.RemoveAt(0); 
+            
+          }
+
+
+
+
+    }
+
 }
+
+
+}
+  
+ 
+}
+
